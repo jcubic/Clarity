bbbb9274207870839ad7457c5322464a77d67a66
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-06-26 21:58:05 +0200

    add miniaml twig+slim setup

--- web/index.php
+++ web/index.php
@@ -1,6 +1,23 @@
 <?php
 
-header("Content-Type: text/plain");
-echo "clarity.pl.eu.org\n";
-echo "PHP " . phpversion() . "\n";
+require __DIR__ . '/vendor/autoload.php';
 
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+$app = AppFactory::create();
+
+$twig = Twig::create(__DIR__ . '/templates');
+$app->add(TwigMiddleware::create($app, $twig));
+
+$app->get('/', function ($request, $response) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'home.html.twig', [
+        'slim_version' => \Slim\App::VERSION,
+        'twig_version' => \Twig\Environment::VERSION,
+        'php_version' => phpversion(),
+    ]);
+});
+
+$app->run();
