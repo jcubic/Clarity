75035a2796173e7d5da87577cabc7189a4758709
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-06-27 11:12:15 +0200

    improve clarity command
    * add bash completion
    * create new template from files

---
+++ theme/bin/clarity-completion.bash
@@ -0,0 +1,49 @@
+# Bash completion for the clarity CLI
+# https://clarity.pl.eu.org
+
+_clarity() {
+    local cur prev commands variants
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    commands="use create install remove list update help"
+    variants="canus dark_canus albus caeruleus lux_caeruleus violaceus lux_violaceus viridis luteus"
+
+    case "$prev" in
+        clarity)
+            COMPREPLY=( $(compgen -W "$commands" -- "$cur") )
+            return
+            ;;
+        use)
+            local themes=""
+            local community_dir="${HOME}/.clarity-icons/community"
+            if [[ -d "$community_dir" ]]; then
+                while IFS= read -r svg; do
+                    local rel="${svg#${community_dir}/}"
+                    themes="$themes @${rel%.svg}"
+                done < <(find "$community_dir" -name "*.svg" -type f 2>/dev/null | sort)
+            fi
+            COMPREPLY=( $(compgen -W "$variants $themes" -- "$cur") )
+            return
+            ;;
+        remove)
+            local themes=""
+            local community_dir="${HOME}/.clarity-icons/community"
+            if [[ -d "$community_dir" ]]; then
+                while IFS= read -r svg; do
+                    local rel="${svg#${community_dir}/}"
+                    themes="$themes @${rel%.svg}"
+                done < <(find "$community_dir" -name "*.svg" -type f 2>/dev/null | sort)
+            fi
+            COMPREPLY=( $(compgen -W "$themes" -- "$cur") )
+            return
+            ;;
+        create)
+            COMPREPLY=( $(compgen -f -X '!*.svg' -- "$cur") )
+            return
+            ;;
+    esac
+}
+
+complete -F _clarity clarity
